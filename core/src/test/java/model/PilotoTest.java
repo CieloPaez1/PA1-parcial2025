6b9a3fbbc3cbef0e5fc60116e5f7208cb51c51a0
CieloPaez <cpaezcampillay@alumnos.undec.edu.ar>
2025-10-14 18:05:49 -0300

    TestPiloto Piloto ExceptionPiloto

--- core/src/test/java/model/PilotoTest.java
+++ core/src/test/java/model/PilotoTest.java
@@ -1,15 +1,18 @@
 package model;
+import exception.ExceptionPiloto;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 import java.time.LocalDateTime;
 import java.util.UUID;
+
 
 public class PilotoTest {
     @Test
     public void FactoryTrue(){
-        Piloto piloto = Piloto.factory(UUID.randomUUID(),"Fanco Colapinto","123456ABC", LocalDateTime.MIN);
+        Piloto piloto = Piloto.factory(UUID.randomUUID(),"Franco Colapinto","123456ABC", LocalDateTime.MIN);
         Assertions.assertNotNull(piloto);
 
 
@@ -17,12 +20,12 @@ public class PilotoTest {
     }
     @Test
     public void FactoryFalse(){
-        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(null,"Fanco Colapinto","123456ABC", LocalDateTime.MIN);});
+        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(null,"Franco Colapinto","123456ABC", LocalDateTime.MIN);});
         Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),null,"123456ABC", LocalDateTime.MIN);});
         Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),"","123456ABC", LocalDateTime.MIN);});
-        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),"Fanco Colapinto","", LocalDateTime.MIN);});
-        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),"Fanco Colapinto",null, LocalDateTime.MIN);});
-        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),"Fanco Colapinto","123456ABC", null);});
-        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),"Fanco Colapinto","123456ABC", LocalDateTime.MAX);});
+        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),"Franco Colapinto","", LocalDateTime.MIN);});
+        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),"Franco Colapinto",null, LocalDateTime.MIN);});
+        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),"Franco Colapinto","123456ABC", null);});
+        Assertions.assertThrows(Exception.class, () -> {Piloto.factory(UUID.randomUUID(),"Franco Colapinto","123456ABC", LocalDateTime.MAX);});
     }
 }
